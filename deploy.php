e412a3da9d3a215e7a4fcaffc5a8c4fdf99e675f
weaving <tanjibo@zhuozhenxuan.com>
2018-07-04 18:32:25 +0800

    项目初始化

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'aws-laradocker-deploy');
+
+// Project repository
+set('repository', 'git@github.com:tanjibo/aws-laradocker-deploy.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('54.146.4.89')
+    ->user('deployer') // 这里填写 deployer
+    // 并指定公钥的位置
+    ->identityFile('~/.ssh/deployerkey')
+    ->set('/Users/weaving/aws-laradock-deploy', '~/');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
